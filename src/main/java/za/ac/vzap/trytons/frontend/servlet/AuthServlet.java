9cb1c998f88677d253892a357246fe1b2332009f
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-07-01 11:50:54 +0200

    Stub for AuthServlet

---
+++ src/main/java/za/ac/vzap/trytons/frontend/servlet/AuthServlet.java
@@ -0,0 +1,9 @@
+package za.ac.vzap.trytons.frontend.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet (name = "AuthServlet" , urlPatterns = "")
+public class AuthServlet extends HttpServlet {
+
+}
