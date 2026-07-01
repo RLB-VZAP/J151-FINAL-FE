31fb999afe5a67408456f3b881fcc80ad101cd3b
Jaunte Kelvin Garcia <kelvinjauntegarcia@gmail.com>
2026-07-01 13:08:56 +0200

    Skeleton of servlet laid out with Login , register and logout

--- src/main/java/za/ac/vzap/trytons/frontend/servlet/AuthServlet.java
+++ src/main/java/za/ac/vzap/trytons/frontend/servlet/AuthServlet.java
@@ -1,9 +1,44 @@
 package za.ac.vzap.trytons.frontend.servlet;
 
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import za.ac.vzap.trytons.frontend.client.AuthRestClient;
 
-@WebServlet (name = "AuthServlet" , urlPatterns = "")
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebServlet (name = "AuthServlet" , urlPatterns = {"/login", "/register", "/logout"})
 public class AuthServlet extends HttpServlet {
+    private final Logger LOGGER = Logger.getLogger(AuthServlet.class.getName());
 
+    @Inject
+    private AuthRestClient authRestClient;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if("/login".equals(request.getServletPath())) {
+            login(request,response);
+        }else if("/register".equals(request.getServletPath())) {
+            register(request,response);
+        }else if("/logout".equals(request.getServletPath())) {
+            logout(request,response);
+        }
+    }
+
+    private void login (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    private void register (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    private void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
+
