af5712a6d2f8aed3d74959b4708d1504cca16cc4
Magdeli-Potgieter <magdelipotgieter.mp@gmail.com>
2026-07-03 13:29:14 +0200

    Fixed some LeaderboardRestClient methods, they still need some parameters.

--- src/main/java/za/ac/vzap/trytons/frontend/client/LeaderboardRestClient.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/LeaderboardRestClient.java
@@ -10,23 +10,18 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Dependent
 public class LeaderboardRestClient {
-    private WebTarget target;
-    private Client client;
+    private String GET_LEADERBOARD_FOR_LEAGUE_PATH = "/leaderboard";
+    private String GET_RANKING_FOR_TEAM_PATH = "/leaderboard";
 
-    private final String baseUrl = "leaderboard";
-    private String GET_LEADERBOARD_FOR_LEAGUE_PATH = baseUrl + "/{leagueId}/rankings";
-    private String GET_RANKING_FOR_TEAM_PATH = baseUrl + "/team/{teamId}";
-
-    ObjectMapper mapper = new ObjectMapper();
     private static final Logger LOG = Logger.getLogger(LeaderboardRestClient.class.getName());
 
     @Inject
     public APIClient apiClient;
 
     public Optional<LeaderboardEntryResponse> getLeaderboardForLeague(){
-        Optional<LeaderboardEntryResponse> response = apiClient.get(GET_LEADERBOARD_FOR_LEAGUE_PATH, LeaderboardEntryResponse.class);
+        String path = GET_LEADERBOARD_FOR_LEAGUE_PATH;
+        Optional<LeaderboardEntryResponse> response = apiClient.get(path, LeaderboardEntryResponse.class);
         if (response.isEmpty()){
             LOG.log(Level.SEVERE, "Could not get leaderboard for league.");
         }
@@ -34,7 +29,8 @@ public class LeaderboardRestClient {
     }
 
     public Optional<LeaderboardEntryResponse> getRankingForTeam(){
-        Optional<LeaderboardEntryResponse> response = apiClient.get(GET_RANKING_FOR_TEAM_PATH, LeaderboardEntryResponse.class);
+        String path = GET_RANKING_FOR_TEAM_PATH;
+        Optional<LeaderboardEntryResponse> response = apiClient.get(path, LeaderboardEntryResponse.class);
         if (response.isEmpty()){
             LOG.log(Level.SEVERE, "Could not get ranking for team.");
         }
