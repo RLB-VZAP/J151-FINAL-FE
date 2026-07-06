b04cbf66784a7f4b90956cf698b6dae3c78b1440
Magdeli-Potgieter <magdelipotgieter.mp@gmail.com>
2026-07-06 09:31:08 +0200

    Finalized LeaderboardRestClient.

--- src/main/java/za/ac/vzap/trytons/frontend/client/LeaderboardRestClient.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/LeaderboardRestClient.java
@@ -1,5 +1,6 @@
 package za.ac.vzap.trytons.frontend.client;
 
+import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Dependent
 public class LeaderboardRestClient {
     private String GET_LEADERBOARD_FOR_LEAGUE_PATH = "/leaderboard";
     private String GET_RANKING_FOR_TEAM_PATH = "/leaderboard";
@@ -17,7 +19,7 @@ public class LeaderboardRestClient {
     public APIClient apiClient;
 
     public Optional<LeaderboardEntryResponse> getLeaderboardForLeague(UUID leagueId) {
-        String path = GET_LEADERBOARD_FOR_LEAGUE_PATH + "/" + leagueId;
+        String path = GET_LEADERBOARD_FOR_LEAGUE_PATH + "/" + leagueId + "/rankings";
         Optional<LeaderboardEntryResponse> response = apiClient.get(path, LeaderboardEntryResponse.class);
         if (response.isEmpty()){
             LOG.log(Level.SEVERE, "Could not get leaderboard for league.");
@@ -25,8 +27,8 @@ public class LeaderboardRestClient {
         return response;
     }
 
-    public Optional<LeaderboardEntryResponse> getRankingForTeam(UUID teamId){
-        String path = GET_RANKING_FOR_TEAM_PATH + "/" + teamId;
+    public Optional<LeaderboardEntryResponse> getRankingForTeam(UUID teamId, UUID leaderboardId) {
+        String path = GET_RANKING_FOR_TEAM_PATH + "/team" + "/" + teamId + "?leaderboardId=" + leaderboardId;
         Optional<LeaderboardEntryResponse> response = apiClient.get(path, LeaderboardEntryResponse.class);
         if (response.isEmpty()){
             LOG.log(Level.SEVERE, "Could not get ranking for team.");
