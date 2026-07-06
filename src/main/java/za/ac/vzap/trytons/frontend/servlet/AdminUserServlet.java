8368a3aabab4b75d28d2bda4be00d8af6efd2271
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-06 11:28:59 +0200

    Created AdminUser REST client, servlet, and JSP stubs.

---
+++ src/main/java/za/ac/vzap/trytons/frontend/servlet/AdminUserServlet.java
@@ -0,0 +1,11 @@
+package za.ac.vzap.trytons.frontend.servlet;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.http.HttpServlet;
+import za.ac.vzap.trytons.frontend.client.AdminUserRestClient;
+
+public class AdminUserServlet extends HttpServlet {
+    //STUB
+    @Inject
+    private AdminUserRestClient adminUserRestClient;
+}
