9552e4e1f0771106a7ecc18a4bf4b5c200e7a1f3
Timothy-Madlala <21mttimothymadlala@gmail.com>
2026-07-09 15:31:47 +0200

    some changes

--- src/main/java/za/ac/vzap/trytons/frontend/servlet/LeagueServlet.java
+++ src/main/java/za/ac/vzap/trytons/frontend/servlet/LeagueServlet.java
@@ -5,7 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import za.ac.vzap.trytons.frontend.client.LeagueRestClient;
 
-@WebServlet(name= "" , urlPatterns = "")
+@WebServlet(name = "LeagueServlet", urlPatterns = {"/leagues"})
 public class LeagueServlet extends HttpServlet {
     @Inject
     private LeagueRestClient leagueRestClient;
