b6d26ad3d013f285dab87fbcb42b801c902bb6ee
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-10 10:14:55 +0200

    FixtureRestClient.java is made and completed with listFixtures and getFixture.

--- src/main/java/za/ac/vzap/trytons/frontend/client/FixtureRestClient.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/FixtureRestClient.java
@@ -2,10 +2,36 @@ package za.ac.vzap.trytons.frontend.client;
 
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Dependent
 public class FixtureRestClient {
-    //STUB
+    private String LIST_FIXTURE ="/fixture";
+    private String GET_FIXTURE = "/fixture";
+   private static final Logger LOG = Logger.getLogger(FixtureRestClient.class.getName());
+
     @Inject
     private APIClient apiClient;
+    public Optional<List<FixtureResponse>> listFixtures(String stausFilter) {
+        StringBuilder path = new StringBuilder(LIST_FIXTURE);
+        if(stausFilter != null && !stausFilter.isEmpty()) {
+            path.append("?staus=").append(stausFilter);
+        }
+        Optional<FixtureResponse[]> response = apiClient.get(path.toString(), FixtureResponse[].class);
+        if(response.isEmpty()){
+            LOG.log(Level.SEVERE, "Unable list fixtures");
+        }
+        return response.map(fixtures -> new ArrayList<>(Arrays.asList(fixtures)));
+    }
+
+    public Optional<FixtureResponse> getFixture(String fixtureId) {
+        String path = GET_FIXTURE + "/" + fixtureId;
+        Optional<FixtureResponse> response = apiClient.get(path,FixtureResponse.class);
+        if(response.isEmpty()){
+            LOG.log(Level.SEVERE, "Unable find fixture");
+        }
+        return response;
+    }
 }
