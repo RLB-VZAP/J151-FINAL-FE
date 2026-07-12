d77a5cf74a59fd294f802f7a4a488e19da2d3845
ChrisbeanCosley <christan1234@gmail.com>
2026-07-12 21:06:04 +0200

    updated UserPointsHistoryResponse in client

--- src/main/java/za/ac/vzap/trytons/frontend/client/UserPointsHistoryResponse.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/UserPointsHistoryResponse.java
@@ -1,4 +1,19 @@
 package za.ac.vzap.trytons.frontend.client;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class UserPointsHistoryResponse {
+
+    private int totals;
+    private List<WeeklyPerformanceResponse> rounds;
+    private Integer ranking;
+
 }
