7f9689cfc1fc053e2e969e9a0f54b7335f121d7b
Sameer-Sacoor <sameersacoorv3@gmail.com>
2026-07-14 19:28:17 +0200

    [W3-FE-LEAGUE-01A] Implement LeagueRestClient
    - Fix logger reference in LeagueRestClient (was AuthRestClient.class, now LeagueRestClient.class)
    - Implement listPublicLeagues, listMyLeagues, getLeague, createLeague,
      joinLeague, listMembers, removeMember
    - Add LeagueRequest, LeagueResponse, JoinLeagueRequest, LeagueMemberResponse DTOs
    
    Files changed:
    - LeagueRestClient.java
    - LeagueRequest.java
    - LeagueResponse.java
    - JoinLeagueRequest.java
    - LeagueMemberResponse.java

---
+++ src/main/java/za/ac/vzap/trytons/frontend/client/JoinLeagueRequest.java
@@ -0,0 +1,15 @@
+package za.ac.vzap.trytons.frontend.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JoinLeagueRequest {
+    private String leagueId;
+    private String leagueCode;
+}
