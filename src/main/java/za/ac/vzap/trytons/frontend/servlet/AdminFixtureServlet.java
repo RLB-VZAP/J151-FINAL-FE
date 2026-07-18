13063ad12280bd39910b4efd4e7646d3160fc1cf
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-18 22:56:24 +0200

    Made it extend the servlet superclass.

--- src/main/java/za/ac/vzap/trytons/frontend/servlet/AdminFixtureServlet.java
+++ src/main/java/za/ac/vzap/trytons/frontend/servlet/AdminFixtureServlet.java
@@ -2,11 +2,10 @@ package za.ac.vzap.trytons.frontend.servlet;
 
 import jakarta.inject.Inject;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import za.ac.vzap.trytons.frontend.client.AdminFixtureRestClient;
 
 @WebServlet(name= "" , urlPatterns = "")
-public class AdminFixtureServlet extends HttpServlet {
+public class AdminFixtureServlet extends AbstractServlet {
     @Inject
     private AdminFixtureRestClient adminFixtureRestClient;
 }
