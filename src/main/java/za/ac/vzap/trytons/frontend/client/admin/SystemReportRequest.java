d9a7f6d883d7d98c08be6ef80646637d0a1ef3ee
Lindsay988 <lindsay.kirstens16@gmail.com>
2026-07-20 18:50:34 +0200

    Made DTO's match the back end DTO's.

--- src/main/java/za/ac/vzap/trytons/frontend/client/admin/SystemReportRequest.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/admin/SystemReportRequest.java
@@ -2,11 +2,12 @@ package za.ac.vzap.trytons.frontend.client.admin;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.util.Map;
 
 @Getter
 @Setter
 public class SystemReportRequest {
     private String reportType;
     private String reportTitle;
-    private String parametersJson;
+    private Map<String , Object > parametersJson;
 }
