05811d8d543d5aee7e16049d45de2c42b3af2610
ChrisbeanCosley <christan1234@gmail.com>
2026-07-21 09:53:55 +0200

    finished AdminFixtureRestClient

--- src/main/java/za/ac/vzap/trytons/frontend/client/fixture/AdminFixtureRestClient.java
+++ src/main/java/za/ac/vzap/trytons/frontend/client/fixture/AdminFixtureRestClient.java
@@ -2,8 +2,12 @@ package za.ac.vzap.trytons.frontend.client.fixture;
 
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,32 +15,43 @@ import za.ac.vzap.trytons.frontend.client.shared.APIClient;
 
 @Dependent
 public class AdminFixtureRestClient {
-    private String UPDATE_STATUS = "/fixtures";
-    private String CREATE_FIXTURE = "/fixtures";
+    private static final String FIXTURES_PATH = "/fixtures";
+
     private static final Logger LOG = Logger.getLogger(AdminFixtureRestClient.class.getName());
     @Inject
     private APIClient apiClient ;
 
     public Optional<FixtureResponse> createFixture(FixtureRequest request){
-        Optional<FixtureResponse> response = apiClient.post(CREATE_FIXTURE,request,FixtureResponse.class);
+        Optional<FixtureResponse> response = apiClient.post(FIXTURES_PATH,request,FixtureResponse.class);
         if(response.isEmpty()){
             LOG.log(Level.SEVERE, "Unable to create fixture");
         }
         return response;
     }
     public Optional<FixtureResponse> updateFixtureStatus(String fixtureId, String status){
-        String path = UPDATE_STATUS + "/" + encode(fixtureId) + "/status?status=" + encode(status);
-        
-        Optional<FixtureResponse> response = apiClient.put(path,null,FixtureResponse.class);
+        String path = FIXTURES_PATH + "/" + encode(fixtureId) + "/status?status=" + encode(status);
+        Optional<FixtureResponse> response = apiClient.put(path, null, FixtureResponse.class);
         if(response.isEmpty()){
             LOG.log(Level.SEVERE, "Unable to update fixture");
         }
         return response;
     }
 
-    private String encode(String value) {
+    public Optional<List<FixtureResponse>> listFixtures(){
+        String path = FIXTURES_PATH;
+
+        Optional<FixtureResponse[]> response = apiClient.get(path, FixtureResponse[].class);
+        if(response.isEmpty()){
+            LOG.log(Level.WARNING, "Unable list fixtures");
+        }
+        return response.map(fixtures -> new ArrayList<>(Arrays.asList(fixtures)));
+
+    }
+
+    public String encode(String value) {
         return URLEncoder.encode(value, StandardCharsets.UTF_8);
     }
 
-    }
+}
+
 
